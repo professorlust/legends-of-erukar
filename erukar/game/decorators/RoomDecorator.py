d8ba1a4802a70009fb459a4fbbfba143cae6f250
etkirsch <etkirsch@gmail.com>
2016-07-03 13:20:24 -0400

    Made some changes to make this work in py3.5

--- erukar/game/decorators/RoomDecorator.py
+++ erukar/game/decorators/RoomDecorator.py
@@ -6,8 +6,8 @@ class RoomDecorator(ProbablisticGenerator):
     def __init__(self, module):
         super().__init__()
 
-        decoration_module = sys.modules[module]
-        poss = [x[1] for x in inspect.getmembers(decoration_module, inspect.isclass)]
+        self.decoration_module = sys.modules[module]
+        poss = [x[1] for x in inspect.getmembers(self.decoration_module, inspect.isclass)]
 
         weights, values = zip(*[(p.Probability, p) for p in poss])
         self.create_distribution(values, weights)
