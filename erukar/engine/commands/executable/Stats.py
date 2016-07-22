8afc2878a0db7cee8bef00806ff4f6ef0f24cb25
Evan Kirsch <etkirsch@gmail.com>
2016-07-22 10:34:18 -0400

    Stats now displays HP/AC

--- erukar/engine/commands/executable/Stats.py
+++ erukar/engine/commands/executable/Stats.py
@@ -2,12 +2,16 @@ from erukar.engine.model import Command
 from erukar.engine.lifeforms import Lifeform
 
 class Stats(Command):
-    header = 'STATS\n----------\n'
+    status = 'STATUS\n----------\nHealth:    {} / {}\nEff. AC:   {}\n'
+    attributes = 'ATTRIBUTES\n----------\n{}'
     stat = "{0:10} {1}"
 
     def execute(self, *_):
         player = self.find_player()
-        stats = '\n'.join([Stats.stat.format(stat.capitalize(), player.character.get(stat)) \
+        lifeform = self.lifeform(player)
+
+        status = self.status.format(lifeform.health, lifeform.max_health, lifeform.calculate_armor_class())
+        atts = '\n'.join([Stats.stat.format(stat.capitalize(), player.character.get(stat)) \
             for stat in Lifeform.attribute_types])
 
-        return Stats.header + stats + '\n'
+        return status + self.attributes.format(atts) + '\n'
