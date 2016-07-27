62bb16cc00dd1bf517da3d3761f80a3530e9873f
Evan Kirsch <etkirsch@gmail.com>
2016-07-27 10:19:53 -0400

    Added probability calculation to Module Decoration

--- erukar/engine/factories/ModuleDecorator.py
+++ erukar/engine/factories/ModuleDecorator.py
@@ -2,15 +2,33 @@ from erukar.engine.factories.ProbablisticGenerator import ProbablisticGenerator
 import sys, inspect
 
 class ModuleDecorator(ProbablisticGenerator):
+    ConditionalProb = 'ProbFrom{}'
+
     def __init__(self, module, generation_parameters):
         super().__init__()
-
+        self.generation_parameters = generation_parameters
         self.decoration_module = sys.modules[module]
         poss = [x[1] for x in inspect.getmembers(self.decoration_module, inspect.isclass)]
 
-        weights, values = zip(*[(p.Probability, p) for p in poss])
+        weights, values = zip(*[(self.calculate_probability(p), p) for p in poss])
         self.create_distribution(values, weights)
 
+    def calculate_probability(self, modifier):
+        prob_weight = 0.0
+        if hasattr(modifier, 'Probability'):
+            prob_weight = getattr(modifier, 'Probability')
+
+        if self.generation_parameters is None:
+            return prob_weight
+
+        for parameter in vars(self.generation_parameters):
+            var_format = self.ConditionalProb.format(parameter.capitalize()) 
+            if hasattr(modifier, var_format):
+                cond_weight = getattr(modifier, var_format) 
+                generation_actual = getattr(self.generation_parameters, parameter)
+                prob_weight += cond_weight * generation_actual 
+
+        return prob_weight
 
     def apply_one_to(self, room):
         '''shortcut to make one and apply it'''
