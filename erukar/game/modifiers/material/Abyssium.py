94111885d0368641e4935ed7e925ef7a012b3db2
etkirsch <etkirsch@gmail.com>
2016-09-07 18:23:23 -0400

    Continuing to work on adding descriptions.

--- erukar/game/modifiers/material/Abyssium.py
+++ erukar/game/modifiers/material/Abyssium.py
@@ -1,13 +1,16 @@
 from erukar.game.modifiers.MaterialModifier import MaterialModifier
 
 class Abyssium(MaterialModifier):
-    Probability = 1000
+    Probability = 400
     ProbabilityFromSanctity = -1.0
     Desirability = 2.0
+    AbsoluteMinimalDescription = "The {EssentialPart} is a reddish black color."
     VisualMinimalDescription = "The {EssentialPart} has been carved out of a dark, reddish black stone that seems to glow a faint red color."
     VisualIdealDescription = "The {EssentialPart} of the {BaseName} has been carved out of a chunk of Abyssium. The dark red Abyssium {EssentialPart} seems to reflect a faint reddish light."
     SensoryMinimalDescription = "The {BaseName} makes you feel uneasy, even at a distance."
     SensoryIdealDescription = "You can sense that the {BaseName} is made of an unholy rock known as Abyssium."
+    DetailedMinimalDescription = "The {EssentialPart} of the {BaseName} has been crafted out of a reddish black stone which emanates a faint red aura. You feel uneasy being in the same room as it."
+    DetailedIdealDescription = "The {BaseName} uses an unholy stone called \"Abyssium\", from which the {EssentialPart} has been carved. The unholy {BaseName} consumes your optimism and fills it with dread."
     Adjective = "Abyssium"
 
     def apply_to(self, item):
--- erukar/game/modifiers/material/Aluminum.py
+++ erukar/game/modifiers/material/Aluminum.py
@@ -1,9 +1,19 @@
 from erukar.game.modifiers.MaterialModifier import MaterialModifier
 
 class Aluminum(MaterialModifier):
-    Probability = 0.4
+    Probability = 400
     ProbabilityFromFabrication = 0.1
     Desirability = 1.0
 
+    AbsoluteMinimalDescription = "The {EssentialPart} is a shiny metal color."
+    VisualMinimalDescription = "The {EssentialPart} has been forged with some sort of worked, gray metal."
+    VisualIdealDescription = "The {EssentialPart} of the {BaseName} has been forged from an unpolished aluminum."
+    SensoryMinimalDescription = "The {BaseName} is lighter than you anticipate."
+    SensoryIdealDescription = "The {BaseName}, specifically the {EssentialPart} is much lighter than you anticipate when first lifting it."
+    DetailedMinimalDescription = "The {EssentialPart} of the {BaseName} is very light, as it has been forged from some sort of unpolished aluminum."
+    DetailedIdealDescription = "The {BaseName}'s {EssentialPart} has been forged from a sturdy and lightweight, yet unpolished, aluminum."
+    Adjective = "Aluminum"
+
     def apply_to(self, item):
+        super().apply_to(item)
         item.name = 'Aluminum ' + item.name
