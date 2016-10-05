229a6473a8bb6675983b7601bfc6d576cab4a8ed
Evan Kirsch <etkirsch@gmail.com>
2016-10-05 11:33:07 -0400

    Added Maximum Stack count; planning on merging into regular item

--- erukar/engine/inventory/StackableItem.py
+++ erukar/engine/inventory/StackableItem.py
@@ -3,6 +3,7 @@ from .Item import Item
 class StackableItem(Item):
     Persistent = True
     PersistentAttributes = ['quantity']
+    MaximumStackQuantity = 100
 
     def __init__(self, name, quantity=1):
         super().__init__(name, name)
@@ -12,11 +13,16 @@ class StackableItem(Item):
         existing_stack = next(self.other_stacks(lifeform.inventory), None)
         if existing_stack:
             existing_stack.quantity += self.quantity
-            lifeform.inventory.remove(self)
+            if existing_stack.quantity <= self.MaximumStackQuantity:
+                lifeform.inventory.remove(self)
+                return
+            difference = existing_stack.quantity - self.MaximumStackQuantity
+            self.quantity = difference
+            existing_stack.quantity = self.MaximumStackQuantity
 
     def other_stacks(self, inventory):
         for item in inventory:
-            if type(item) == type(self) and item is not self:
+            if type(item) == type(self) and item is not self and item.quantity < self.MaximumStackQuantity:
                 yield item
 
     def on_inventory(self):
---
+++ erukar/engine/inventory/TieredKey.py
@@ -0,0 +1,6 @@
+from erukar.engine.inventory.Key import Key
+
+class TieredKey(Key):
+    BriefDescription = "a {tier_material} key"
+    BaseName = "{tier_material} Key"
+
