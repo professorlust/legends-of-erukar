ff0e5c2fb49c6982182c0e3d5d0cdf85058649a5
Evan Kirsch <etkirsch@gmail.com>
2016-10-06 15:23:33 -0400

    fixed a minor issue with nomenclature to TieredLock

--- erukar/engine/environment/TieredLock.py
+++ erukar/engine/environment/TieredLock.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class TieredLock(Lock):
     Tiers = [
-        # Material, Prevalence
+        # Material, Difficulty Prevalence
         ('Iron',1),
         ('Steel',2),
         ('Copper',4),
@@ -18,5 +18,7 @@ class TieredLock(Lock):
     def __init__(self, difficulty):
         possibilities, weights = zip(*[(tier, 1/(value*2)) for tier,value in self.Tiers])
         bins, values = Random.create_random_distribution(possibilities, weights)
-        random = float(np.random.beta(10/difficulty, 5))
-        self.tier = Random.get_from_custom_distribution(random, bins, values)
+        # Use Beta Dist to bind on range [0,1]; this tightly skews the distribution 
+        # from lower difficulties to higher difficulties 
+        random_value = float(np.random.beta(10/difficulty, 5))
+        self.tier = Random.get_from_custom_distribution(random_value, bins, values)
