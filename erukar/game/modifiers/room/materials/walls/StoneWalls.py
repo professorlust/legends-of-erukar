ec7401defdb6829933065f2b6168ff40a4d8262d
Evan Kirsch <etkirsch@gmail.com>
2016-10-12 15:51:10 -0400

    Added Observations to StoneWalls to serve as example

--- erukar/game/modifiers/room/materials/walls/StoneWalls.py
+++ erukar/game/modifiers/room/materials/walls/StoneWalls.py
@@ -1,10 +1,20 @@
 from erukar.engine.model import Modifier
 from erukar.engine.environment import Room
 from erukar.game.modifiers.RoomModifier import RoomModifier
+from erukar.engine.model.Observation import Observation
 
 class StoneWalls(RoomModifier):
     ProbabilityFromFabrication = -0.3
+
+    Glances = [
+        Observation(acuity=0, sense=0, result="made of stone"),
+    ]
+
+    Inspects = [
+        Observation(acuity=0, sense=0, result="This wall is made of stone.")
+    ]
+
     def apply_to(self, room):
         for wall in room.walls():
-            wall.BriefDescription = "stone"
-            wall.description = "This wall is made of stone"
+            wall.Glances = wall.Glances + self.Glances
+            wall.Inspects = wall.Inspects + self.Inspects
