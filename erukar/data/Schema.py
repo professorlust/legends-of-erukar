d101068230a6ae35822acde3bcf25ac171cf9c34
Evan Kirsch <etkirsch@gmail.com>
2016-12-08 15:37:21 -0500

    Added inheritance to Characters and Creatures in the DB; add Lifeforms

--- erukar/data/Schema.py
+++ erukar/data/Schema.py
@@ -1,14 +1,14 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base, ConcreteBase
+from sqlalchemy import Column, Float, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.dialects.postgresql import ARRAY, JSON
 
 Base = declarative_base()
 
 class EquippedItem(Base):
     __tablename__ = 'equippeditems'
 
-    character_id    = Column(Integer, ForeignKey('characters.id'), primary_key=True)
+    lifeform_id     = Column(Integer, ForeignKey('lifeforms.id'), primary_key=True)
     item_id         = Column(Integer, ForeignKey('items.id'), primary_key=True)
     item            = relationship("Item")
     equipment_slot  = Column(String)
@@ -23,27 +23,59 @@ class Player(Base):
     characters  = relationship("Character")
 
 
-class Character(Base):
-    __tablename__ = 'characters'
+class Lifeform(Base):
+    __tablename__ = 'lifeforms'
+    __mapper_args_ = { 
+        'polymorphic_identity': 'lifeforms',
+        'polymorphic_on': type
+    }
 
     id          = Column(Integer, primary_key=True)
+    type        = Column(String(50))
     deceased    = Column(Boolean, default=False)
-    name        = Column(String)
     max_health  = Column(Integer, default=4)
-    health      = Column(Integer)
+    name        = Column(String,  default="unnamed")
+    health      = Column(Integer, default=4)
     strength    = Column(Integer, default=0)
     dexterity   = Column(Integer, default=0)
     vitality    = Column(Integer, default=0)
     acuity      = Column(Integer, default=0)
     sense       = Column(Integer, default=0)
     resolve     = Column(Integer, default=0)
+
     level       = Column(Integer, default=1)
     experience  = Column(Integer, default=0)
-    stat_points = Column(Integer, default=15)
 
     equipment   = relationship("EquippedItem", cascade="all, delete-orphan")
     inventory   = relationship("Item", cascade="all, delete-orphan")
     effects     = relationship("Effect", cascade="all, delete-orphan")
+    modifiers   = Column(ARRAY(String))
+
+
+class Creature(Lifeform):
+    __tablename__ = 'creatures'
+    __mapper_args_ = { 
+        'polymorphic_identity': 'creatures',
+    }
+
+    id          = Column(Integer, ForeignKey('lifeforms.id'), primary_key=True)
+    str_ratio   = Column(Float, default=0.1667)
+    dex_ratio   = Column(Float, default=0.1667)
+    vit_ratio   = Column(Float, default=0.1667)
+    acu_ratio   = Column(Float, default=0.1667)
+    sen_ratio   = Column(Float, default=0.1667)
+    res_ratio   = Column(Float, default=0.1667)
+    elite_points= Column(Float, default=0.0)
+
+
+class Character(Lifeform):
+    __tablename__ = 'characters'
+    __mapper_args_ = { 
+        'polymorphic_identity': 'characters',
+    }
+
+    id          = Column(Integer, ForeignKey('lifeforms.id'), primary_key=True)
+    stat_points = Column(Integer, default=15)
     player_id   = Column(Integer, ForeignKey('players.id'), nullable=False)
     player      = relationship("Player", foreign_keys=[player_id])
 
@@ -53,8 +85,8 @@ class Item(Base):
 
     id              = Column(Integer, primary_key=True)
     item_type       = Column(String)
-    character_id    = Column(Integer, ForeignKey('characters.id'))
-    character       = relationship("Character", foreign_keys=[character_id])
+    lifeform_id     = Column(Integer, ForeignKey('lifeforms.id'))
+    lifeform        = relationship("Lifeform", foreign_keys=[lifeform_id])
     material_type   = Column(String)
     modifiers       = relationship("Modifier", cascade="all, delete-orphan")
     item_attributes = Column(JSON, nullable=True)
@@ -74,5 +106,6 @@ class Effect(Base):
 
     id              = Column(Integer, primary_key=True)
     effect_type     = Column(String)
-    character_id    = Column(Integer, ForeignKey('characters.id'))
-    character       = relationship("Character", foreign_keys=[character_id])
+    lifeform_id     = Column(Integer, ForeignKey('lifeforms.id'))
+    lifeform        = relationship("Lifeform", foreign_keys=[lifeform_id])
+
