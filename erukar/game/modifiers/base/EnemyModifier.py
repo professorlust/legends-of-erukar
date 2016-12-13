008c15619c9ce110bdeb4ab39d06df991d7b41b6
Evan Kirsch <etkirsch@gmail.com>
2016-12-13 09:18:25 -0500

    Added auto-Deprecating enemy modifiers

--- erukar/game/modifiers/base/EnemyModifier.py
+++ erukar/game/modifiers/base/EnemyModifier.py
@@ -4,7 +4,13 @@ from erukar.engine.lifeforms import Enemy
 class EnemyModifier(Modifier):
     PermissionType = Modifier.ALL_PERMITTED
     PermittedEntities = [Enemy]
+    DeprecatesModifiers = []
 
     def apply_to(self, enemy):
+        enemy.modifiers = list(self.deprecate_existing_modifiers(enemy.modifiers))
         enemy.modifiers.append(self)
 
+    def deprecate_existing_modifiers(self, modifiers):
+        for mod in modifiers:
+            if not any(x for x in self.DeprecatesModifiers if issubclass(type(mod), x)):
+                yield x
--- erukar/game/modifiers/enemy/static/EpicQuestTarget.py
+++ erukar/game/modifiers/enemy/static/EpicQuestTarget.py
@@ -1,4 +1,7 @@
 from erukar.game.modifiers.base.EnemyModifier import EnemyModifier
 
 class EpicQuestTarget(EnemyModifier):
-    pass
+    def apply_to(self, enemy):
+        enemy.modifiers = [x for x in enemy.modifiers 
+                           if not issubclass(type(x), erukar.game.modifiers.enemy.static.QuestTarget)]
+        enemy.modifiers.append(self)
