d9dc080f1bdcd85362b3f3dab31aa6b2c984ea09
Evan Kirsch <etkirsch@gmail.com>
2017-02-13 11:07:19 -0500

    removed vestiges of old weapon randomization system

--- erukar/game/modifiers/inventory/random/__init__.py
+++ erukar/game/modifiers/inventory/random/__init__.py
@@ -6,19 +6,14 @@ from .ChanceToInflictCondition import ChanceToInflictCondition
 from .Dark import Dark
 from .Dull import Dull
 from .Enhancement import Enhancement
-from .Fine import Fine
 from .Glowing import Glowing
-from .Grand import Grand
-from .Greater import Greater
 from .Holy import Holy
-from .Large import Large
 from .Luminous import Luminous
 from .NoModifier import NoModifier
 from .Reinforced import Reinforced
 from .Rusty import Rusty
 from .Serrated import Serrated
 from .Sharp import Sharp
-from .Small import Small
 from .Swift import Swift
 from .Unholy import Unholy
 from .Unwieldy import Unwieldy
@@ -32,19 +27,14 @@ __all__ = [
     "Dark", 
     "Dull", 
     "Enhancement", 
-    "Fine", 
     "Glowing", 
-    "Grand", 
-    "Greater", 
     "Holy", 
-    "Large", 
     "Luminous", 
     "NoModifier", 
     "Reinforced", 
     "Rusty", 
     "Serrated", 
     "Sharp", 
-    "Small", 
     "Swift", 
     "Unholy", 
     "Unwieldy"
