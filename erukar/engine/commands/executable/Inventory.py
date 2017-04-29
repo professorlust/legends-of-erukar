fd8972803c37725cf47e16d8bfe7be834fe334e8
Evan Kirsch <etkirsch@gmail.com>
2017-04-29 12:04:32 -0400

    Armors and Weapons output detaiols as appropriate

--- erukar/engine/commands/executable/Inventory.py
+++ erukar/engine/commands/executable/Inventory.py
@@ -7,25 +7,40 @@ class Inventory(Command):
         pass
 
     def format_item_json(item):
-        return json.dumps({
+        object_output = {
             'id': str(item.uuid),
             'alias': item.alias(),
             'durability': {'current': item.durability(), 'max': item.max_durability()},
             'desirabilityRating': item.rarity(),
             'slots': item.EquipmentLocations,
-            'details': list(Inventory.generate_list_of_details(item)) #[
-#               {type: 'property', title: '', value: '3 to 4 Slashing Damage'},
-#               {type: 'standard', title: 'Weight', value: '10.50 levts'},
-#               {type: 'standard', title: 'Iron', value: 'A strong base metal -- hard to forge and fairly heavy but resilient'},
-#               {type: 'legendary', title: 'Legendary Magekiller', value: 'Has a 25% chance to burst stored hexcells'},
-#               {type: 'description', title: '', value: 'The pole of Ankithalis is a carved Iurwood reinforced with rings of platinum and adorned by sapphires. In its entirety, Ankithalis is roughly 6 feet and 3 inches long. The head of the halberd bears the insignia of King Tyroche Alar of Iuria, the Renegade King. Various symbols describe how Browr Trowr, Galadrast advocate of King Alar, used the weapon in defense of his liege.'}
-           #]
-        })
+            'details': list(Inventory.generate_list_of_details(item)) 
+        }
+
+        if isinstance(item, erukar.engine.inventory.Weapon):
+            object_output['damage'] = {}
+            for name, d_range in Inventory.weapon_details(item):
+                object_output['damage'][name] = d_range
+
+        if isinstance(item, erukar.engine.inventory.Armor):
+            object_output['protection'] = {}
+            for name, mit in Inventory.armor_details(item):
+                object_output['protection'][name] = mit
+
+        return json.dumps(object_output)
 
     def generate_list_of_details(item):
         yield Inventory.format_modifier(item.material)
         for modifier in item.modifiers:
             yield Inventory.format_modifier(modifier)
+        # Description here
+
+    def weapon_details(item):
+        for damage in item.damages:
+            yield damage.name.capitalize(), '{} to {}'.format(damage.damage[0], damage.damage[1])
+
+    def armor_details(item):
+        for mit in item.DamageMitigations:
+            yield mit.capitalize(), {'deflection': item.DamageMitigations[mit][1], 'mitigation': item.DamageMitigations[mit][0]}
 
     def format_modifier(modifier):
         return {
--- tests/engine/commands/test_inventory.py
+++ tests/engine/commands/test_inventory.py
@@ -2,10 +2,18 @@ from erukar import *
 import unittest, erukar
 
 class InventoryTests(unittest.TestCase):
-    def test_format_item_json(self):
+    def test_format_weapon_json(self):
         item = Sword()
         erukar.game.modifiers.material.Steel().apply_to(item)
         result = Inventory.format_item_json(item)
-        print(result)
 
         self.assertIn(str(item.uuid), result)
+        self.assertIn('Slashing', result)
+
+    def test_format_armor_json(self):
+        item = Hauberk()
+        erukar.game.modifiers.material.Steel().apply_to(item)
+        result = Inventory.format_item_json(item)
+
+        self.assertIn(str(item.uuid), result)
+        self.assertIn('Slashing', result)
