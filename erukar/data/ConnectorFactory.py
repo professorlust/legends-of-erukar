474121a4bb20837c8cbf072bca95fbeae82537ea
Evan Kirsch <etkirsch@gmail.com>
2017-06-13 15:04:02 -0400

    Started working on heroku integration

--- erukar/data/ConnectorFactory.py
+++ erukar/data/ConnectorFactory.py
@@ -3,11 +3,11 @@ from erukar.data.Connector import Connector
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 class ConnectorFactory:
-    def __init__(self, passwd="thisisnottherealpassword"):
-        self.connection_string = "postgres+pygresql://postgres:{}@localhost:5432/loedev".format(passwd)
-
+    def __init__(self,username="postgres",passwd="nottherealpass"):
+        self.connection_string = os.environ.get('DATABASE_URL',"postgres+pygresql://{}:{}@localhost:5432/loedev".format(username, passwd))
     def establish_connection(self):
         self.engine = sqlalchemy.create_engine(self.connection_string)
         self.Session = sessionmaker(bind=self.engine)
