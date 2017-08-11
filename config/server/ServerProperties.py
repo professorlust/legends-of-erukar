294cb5236c7baed333909abb2ff36be59eb475d0
Evan Kirsch <etkirsch@gmail.com>
2017-08-11 19:05:11 -0400

    Added a bunch of server properties

--- config/server/ServerProperties.py
+++ config/server/ServerProperties.py
@@ -1,11 +1,13 @@
-def configure(shard):
-    shard.properties.Name = 'Alar\'s Realm'
-    shard.properties.Url = 'http://alars-realm.herokuapp.com'
-    shard.properties.MaxPlayers = 10
-    shard.properties.Description = 'A basic LoE server'
+import os
 
-    shard.properties.AdminDetails = 'Legends of Erukar Official (admin@loe.dev)'
-    shard.properties.PermaDeath = False
+def configure(shard):
+    shard.properties.Name = os.environ.get('SERVER_NAME', 'Default Server Name')
+    shard.properties.Url = os.environ.get('SERVER_URL', 'http://alars-realm.herokuapp.com')
+    shard.properties.MaxPlayers = int(os.environ.get('MAX_PLAYERS',10))
+    shard.properties.Description = os.environ.get('DESCRIPTION', 'A basic LoE server')
+
+    shard.properties.AdminDetails = os.environ.get('ADMIN_DETAILS', 'Legends of Erukar Official (admin@loe.dev)')
+    shard.properties.PermaDeath = True
 
     shard.properties.StartingWealth = 1000
     shard.properties.StartingStatPoints = 20
