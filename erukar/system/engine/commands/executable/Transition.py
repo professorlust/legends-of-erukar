ab44d5e1f915fbd3b0b2f3e5f962436955601ca5
Evan Kirsch <etkirsch@gmail.com>
2017-09-07 15:52:19 -0400

    argh

--- erukar/system/engine/commands/executable/Transition.py
+++ erukar/system/engine/commands/executable/Transition.py
@@ -9,10 +9,10 @@ class Transition(ActionCommand):
         target
     '''
     def perform(self):
-        if self.invalid('target'): return self.fail('Target is invalid')
+        if self.invalid('interaction_target'): return self.fail('Target is invalid')
 
-        if not isinstance(self.args['target'], TransitionPiece):
+        if not isinstance(self.args['interaction_target'], TransitionPiece):
             return self.fail('Transition target is not a TransitionPiece')
 
-        self.args['player_info'].mark_for_transition(self.args['target'].destination)
+        self.args['player_info'].mark_for_transition(self.args['interaction_target'].destination)
         return self.succeed()
