fd6f62fa0c690f41c7291a9554ab537c87df77dd
Evan Kirsch <etkirsch@gmail.com>
2017-09-07 11:20:21 -0400

    Fixed more bugs in BarlenRegion

--- config/world/regions/BarlenRegion.py
+++ config/world/regions/BarlenRegion.py
@@ -1,7 +1,8 @@
 import erukar
+from erukar.system.engine import EnvironmentProfile, Sector, Region
 
 def create():
-    barlen = erukar.system.engine.Region()
+    barlen = Region()
     barlen.name = "Greater Barlen Region"
     barlen.description = "A fertile area, known best for its vast barley and wheat fields. The seat of this region is a large town known as Barlen whose economy consists mostly on agriculture taxes and exports of the barley harvest."
     barlen.sectors = [
@@ -32,6 +33,6 @@ def create_razorwoods_camp(region):
     return sector
 
 def create_sector_template(region=None):
-    sector = erukar.system.engine.Sector(region)
+    sector = Sector(region)
     sector.environment_profile = EnvironmentProfile.Woodlands()
     return sector
