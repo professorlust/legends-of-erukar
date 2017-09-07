bdbd662d69be503f7e5a1b75e5cb5076ea33fc16
Evan Kirsch <etkirsch@gmail.com>
2017-09-07 11:18:36 -0400

    Fixed bug in BarlenRegion creation script

--- config/world/regions/BarlenRegion.py
+++ config/world/regions/BarlenRegion.py
@@ -5,8 +5,8 @@ def create():
     barlen.name = "Greater Barlen Region"
     barlen.description = "A fertile area, known best for its vast barley and wheat fields. The seat of this region is a large town known as Barlen whose economy consists mostly on agriculture taxes and exports of the barley harvest."
     barlen.sectors = [
-        create_barlen_outskirts(),
-        create_razorwoods_camp()
+        create_barlen_outskirts(barlen),
+        create_razorwoods_camp(barlen)
     ]
     barlen.sector_limits = list(acceptable_bounds())
     barlen.sector_template = create_sector_template()
@@ -19,7 +19,7 @@ def acceptable_bounds():
             for beta in range(-1, 3):
                 yield (x, alpha, beta)
 
-def create_barlen_outskirts():
+def create_barlen_outskirts(region):
     sector = create_sector_template(region) 
     sector.name = 'Barlen Town Outskirts'
     sector.set_coordinates(0, 0, 0)
